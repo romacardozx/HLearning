1583df4c8087bc67f4c3608ef0b07d812ec52e27
gonzalohlh <gonzalo.galdame@gmail.com>
2021-10-28 03:27:58 +0400

    carpeta Courses y solo archivo courses.jsx creados y store creada

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar, Collapse} from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, IconButton, Toolbar, Collapse } from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Hlearning from "../../images/Hlearning.png";
-import { Link as Scroll } from 'react-scroll';
+import { Link as Scroll } from "react-scroll";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    fontFamily: 'Nunito',
-    background: 'black'
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    fontFamily: "Nunito",
+    background: "black",
   },
   appbar: {
-    background: 'none',
+    background: "none",
   },
   appbarWrapper: {
-    width: '100%',
-    margin: '0 auto',
+    width: "100%",
+    margin: "0 auto",
   },
   appbarTitle: {
-    flexGrow: '1',
+    flexGrow: "1",
   },
   icon: {
-    color: '#fff',
-    fontSize: '2rem',
+    color: "#fff",
+    fontSize: "2rem",
   },
   colorText: {
-    color: '#38b6ff',
+    color: "#38b6ff",
   },
   colorText2: {
-    color: '#38b6ff',
-    fontSize: '2rem'
+    color: "#38b6ff",
+    fontSize: "2rem",
   },
   container: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   title: {
-    color: '#fff',
-    fontSize: '4.5rem',
+    color: "#fff",
+    fontSize: "4.5rem",
   },
   goDown: {
-    color: '#38b6ff',
-    fontSize: '4rem',
+    color: "#38b6ff",
+    fontSize: "4rem",
   },
 }));
 export default function Header() {
@@ -57,12 +57,8 @@ export default function Header() {
     <div className={classes.root} id="header">
       <AppBar className={classes.appbar} elevation={0}>
         <Toolbar className={classes.appbarWrapper}>
-            <img
-            height="80"
-            width="80" 
-            src={Hlearning} 
-            alt="HLearning" />
-            <span className={classes.colorText2}>HLearning</span>
+          <img height="80" width="80" src={Hlearning} alt="HLearning" />
+          <span className={classes.colorText2}>HLearning</span>
         </Toolbar>
       </AppBar>
 
@@ -73,7 +69,8 @@ export default function Header() {
       >
         <div className={classes.container}>
           <h1 className={classes.title}>
-            Welcome<br />
+            Welcome
+            <br />
             Start <span className={classes.colorText}> Learning.</span>
           </h1>
           <Scroll href="/home" smooth={true}>
