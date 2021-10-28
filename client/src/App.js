2550cf6c30b0f1a1a2159c76d172bfdc0b5af7e7
gonzalohlh <gonzalo.galdame@gmail.com>
2021-10-28 18:45:08 +0400

    componente Card creado

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import { Route } from 'react-router-dom'
-import Landing from './components/Landing/Landing'
+import { Route } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
+import CardCourse from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
-        <Route exact path="/" component={Landing} />
-     
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/card" component={CardCourse} />
     </div>
   );
 }
--- client/src/components/Courses/Courses.jsx
+++ client/src/components/Courses/Courses.jsx
@@ -1,1 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllCurses } from ".../actions/getAllCurses";
+import { Link } from "react-router-dom";
+
+export default function Courses() {
+  const dispatch = useDispatch();
+  const allCourses = useSelector((state) => state.courses);
+}
