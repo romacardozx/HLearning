dca66c1ab24e637045a7b75f87da9d17da800ce8
CeciliaWoi <cecilia@woitasen.com.ar>
2021-10-29 19:24:52 -0300

    ruta getAllCourses Ok

--- api/src/routes/coursesRoutes.js
+++ api/src/routes/coursesRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const express = require('express');
 const createCourse = require('../controllers/courses/createCourse');
+const getAllCourses = require('../controllers/courses/getAllCourses')
 
 const router = Router();
 router.use(express.json());
+router.get('/', getAllCourses)
 router.post('/createCourse', createCourse);
 
 
