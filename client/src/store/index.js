2fe9ed38f35d7454c15ad060590c9e3e011742a9
Maxi <requenamaximiliano@gmail.com>
2021-10-29 10:11:09 -0300

    'clear node'

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import rootReducer from '../reducer';
+import rootReducer from '../reducer/reducer.js';
 
 export const store = createStore(
-  /* rootReducer, */
-  composeWithDevTools(rootReducer, applyMiddleware(thunk))
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
