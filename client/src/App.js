74977de7d72bacd38482b5974f224f71013f1c0b
gonzalohlh <gonzalo.galdame@gmail.com>
2021-10-30 05:21:07 +0400

    comp Courses SearchBar y Paginate renderizados

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import { Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Landing from "./components/Landing/Landing";
-import Card from "./components/Card/Card";
-//import Courses from "./components/Courses/Courses";
-import Privacy from "./components/Privacy/Privacy"
+import Privacy from "./components/Privacy/Privacy";
+import Courses from "./components/Courses/Courses";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Landing} />
       <Route exact path="/home" component={Home} />
-      <Route exact path="/card" component={Card} />
-      {/* <Route exact path="/card" component={Courses} /> */}
+      <Route exact path="/courses" component={Courses} />
       <Route exact path="/privacy" component={Privacy} />
     </div>
   );
--- client/src/components/ViewBase/view-base.jsx
+++ client/src/components/ViewBase/view-base.jsx
@@ -2,17 +2,25 @@ import "./styleViewBase.css";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 
-export default function ViewBase({carousel, content}) {
-    return (
-        <div className="base-container">
-            <div className={ carousel? "header-container with-carousel" : "header-container"}>
-                <div className="navBar-container"><NavBar/></div>
-                {carousel &&<div className="carousel-container">{ carousel }</div>}
-            </div>
-            <div className="body-container">
-               <div >{content}</div>
-            </div>
-            <div className="footer-container"><Footer/></div>
+export default function ViewBase({ carousel, content }) {
+  return (
+    <div className="base-container">
+      <div
+        className={
+          carousel ? "header-container with-carousel" : "header-container"
+        }
+      >
+        <div className="navBar-container">
+          <NavBar />
         </div>
-    );
+        {carousel && <div className="carousel-container">{carousel}</div>}
+      </div>
+      <div className="body-container">
+        <div>{content}</div>
+      </div>
+      <div className="footer-container">
+        <Footer />
+      </div>
+    </div>
+  );
 }
