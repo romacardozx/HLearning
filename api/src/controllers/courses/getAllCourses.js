cabfcd6d25e4289ca3d5c4cb1c4f25a536de7f0c
LihueMaidanaAvaca <lihuevida@gmail.com>
2021-10-31 18:45:21 -0300

    ordenamiento

--- api/src/controllers/courses/getAllCourses.js
+++ api/src/controllers/courses/getAllCourses.js
@@ -1,11 +1,64 @@
 const Course = require("../../models/Course");
 
-module.exports = async (_req, res, next) => {
+module.exports = async ( req, res, next) => {
   const courses = await Course
     .find()
     .populate("categories", "name -_id");
   try {
-    res.json(courses);
+    let { abc } = req.query; 
+    
+    if(abc){
+      if(order === "A-Z" || !order || order === ""){
+        coursesOrder = courses.sort((a,b) =>{
+            if(a.name > b.name) return 1;
+            if(b.name > a.name) return -1; 
+            return 0; 
+        })
+        return res.send(coursesOrder)
+      } if(order === "Z-A" || !order || order === ""){
+      coursesOrder = courses.sort((a,b) =>{
+          if(a.name > b.name) return -1;
+          if(b.name > a.name) return 1; 
+          return 0;
+      })
+      return res.send(coursesOrder)
+      }
+    }
+    if(score){
+      if(score === "Asc" || !score || score ===""){
+          orderScore = courses.sort((a,b) => {
+              if(a.score > b.score) return 1;
+              if (a.score < b.score)return -1;
+              return 0;    
+          })
+          return res.send(orderScore)
+        } if(score === "Desc" || !score || score ===""){
+            orderScore = courses.sort((a,b) => {
+              if(a.score > b.score) return -1;
+              if (a.score < b.score)return 1;
+              return 0;    
+          })
+      return res.send(orderScore);
+      }
+    }
+    if(price){
+      if(price === "Asc" || !price || price ===""){
+        orderPrice = courses.sort((a,b) => {
+            if(a.price > b.price) return 1;
+            if (a.price < b.price)return -1;
+            return 0;    
+        })
+        return res.send(orderPrice)
+      } if(price === "Desc" || !price || price ===""){
+          orderPrice = courses.sort((a,b) => {
+            if(a.price > b.price) return -1;
+            if (a.price < b.price)return 1;
+            return 0;    
+        })
+    return res.send(orderPrice);
+    }
+    }else res.json(courses);
+      
   } catch (err) {
     next(err);
   }
