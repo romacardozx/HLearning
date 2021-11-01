64585e6848c62b238f1e6bd88105df632de48db2
Roman Cardozo <79112075+romacardozx@users.noreply.github.com>
2021-11-01 17:11:25 -0300

    Revert "Micaela/componente detail"

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,11 @@
-import { Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import Landing from "./components/Landing/Landing";
-import Courses from "./components/Courses/Courses";
-import Privacy from "./components/Privacy/Privacy"
-import Contact from "./components/Contact/Contact";
-import Detail from './components/Detail/Detail';
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register"
-
-
+import { Route } from 'react-router-dom'
+import Landing from './components/Landing/Landing'
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={Landing}/>
-      <Route exact path="/home" component={Home}/>
-      <Route exact path="/courses" component={Courses}/>
-      <Route exact path="/register" component={Register}/>
-      <Route exact path="/login" component={Login}/>
-      <Route exact path="/privacy" component={Privacy}/>
-      <Route exact path="/contact" component={Contact}/>
-      <Route exact path="/courses/:id" component={Detail}/>
+        <Route exact path="/" component={Landing} />
+     
     </div>
   );
 }
