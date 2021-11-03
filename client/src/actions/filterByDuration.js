c5c6abd6b21268bec7f268fc9800373a50103ba6
gonzalohlh <gonzalo.galdame@gmail.com>
2021-11-04 00:33:56 +0400

    corregido el header del filterByDuration

--- client/src/actions/filterByDuration.js
+++ client/src/actions/filterByDuration.js
@@ -1,21 +1,5 @@
 import axios from "axios";
 
-<<<<<<< HEAD
-
-function filterByDuration(category){
-
-    return async function(dispatch){
-        try {
-            var json = await axios(`/courses?category=${category?category:""}`);
-            return dispatch({
-                type: "FILTER_BY_DURATION",
-                payload: json.data
-            })
-            
-        } catch (error) {
-            console.log("error", error)
-        }
-=======
 function filterByDuration(category) {
   return async function (dispatch) {
     try {
@@ -26,7 +10,6 @@ function filterByDuration(category) {
       });
     } catch (error) {
       console.log("error", error);
->>>>>>> f95eea6c6840bed92d915bbba67b27ba214f70db
     }
   };
 }
