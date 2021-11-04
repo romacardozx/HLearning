b9cc6018205b1d3b33c933bf4467ddb962ed610e
gonzalohlh <gonzalo.galdame@gmail.com>
2021-11-04 23:05:03 +0400

    primer prueba en el carrusel

--- client/src/components/Carrusel/Carrusel.js
+++ client/src/components/Carrusel/Carrusel.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
+/* import { useSelector } from "react-redux";
+import { getCourses5Stars } from "../../actions/getCourses5Stars"; */
 import "./Slider.css";
-import images from "./Carruselimg.json";
+/* import images from "./Carruselimg.json"; */
+import images from "./coursesProvisorios.json";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function Carrusel() {
+  /* const courses5Stars = useSelector((state) => state.getCourses5Stars); */
   const [slideIndex, setSlideIndex] = useState(1);
   function nextSlide() {
     if (slideIndex !== images.length) {
@@ -14,9 +18,9 @@ export default function Carrusel() {
   }
 
   useEffect(() => {
-    setTimeout(nextSlide, 5000);// eslint-disable-next-line
+    setTimeout(nextSlide, 3000); // eslint-disable-next-line
   }, [slideIndex]);
-
+  /* 
   return (
     <div className="container-slider">
       {images.map((obj, index) => {
@@ -26,6 +30,20 @@ export default function Carrusel() {
             className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
           >
             <img src={obj.image} alt={obj.adress} />
+          </div>
+        );
+      })}
+    </div>
+  ); */
+  return (
+    <div className="container-slider">
+      {images.map((obj, index) => {
+        return (
+          <div
+            key={index}
+            className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
+          >
+            <img src={obj.img} alt={obj.title} />
           </div>
         );
       })}
