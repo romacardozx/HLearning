4b7e9ceba5fe7e8203fb5491b21823a6a1ec12ef
Bellantra <yamilapaez2@hotmail.com>
2021-11-05 20:32:42 +0100

    Rutas Category refactorizadas con status

--- api/src/controllers/courses/getCourseById.js
+++ api/src/controllers/courses/getCourseById.js
@@ -4,7 +4,7 @@ const Category = require("../../models/Category");
 module.exports = async (req, res, next) => {
   const { id } = req.params; 
   try {
-    let course = await Course.findById({_id:id})
+    let course = await Course.findById({_id:id})  //Chechu...aca devuelve sin contemplar el status!!
     course = await Category.populate(course,{path:'categories'})
     res.json(course);
     } catch (err) {    
