639e3876998255dffeb4232f86440a4bece4d6df
Micaela <80907969+MFOSS13@users.noreply.github.com>
2021-11-06 13:07:06 -0300

    cambios css detail y deletecourse

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import PruebaAdm from "./components/PanelAdmin/PruebaAdm"
 import UserProfile from "./components/UserProfile/UserProfile";
+import DeleteCourse from "./components/PanelAdmin/DeleteCourse";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/courses/:id" component={Detail} />
       <Route exact path="/prueba" component={PruebaAdm} />
+      <Route exact path="/prueba2" component={DeleteCourse} />
     </div>
   );
 }
