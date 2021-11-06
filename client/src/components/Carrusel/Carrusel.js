af4f2987ed0c07658725413749b59d437c6833ab
gonzalohlh <gonzalo.galdame@gmail.com>
2021-11-06 22:09:57 +0400

    carrusel 5 estrellas eliminado y renderizando cards

--- client/src/components/Carrusel/Carrusel.js
+++ client/src/components/Carrusel/Carrusel.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCoursesScore } from "../../redux/actions/getCoursesScore";
+import Card from "../Card/Card";
 import "./Slider.css";
-import images from "./coursesProvisorios.json";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
@@ -23,95 +22,53 @@ const Img = styled("img")({
 
 export default function Carrusel() {
   const dispatch = useDispatch();
-  const [slideIndex, setSlideIndex] = useState(1);
-  function nextSlide() {
-    if (slideIndex !== images.length) {
-      setSlideIndex(slideIndex + 1);
-    } else if (slideIndex === images.length) {
-      setSlideIndex(1);
-    }
-  }
-  const coursesTop = useSelector((state) => state.getCourses.getAllCourses);
-  /* console.log("COURSES TOP"  coursesTop); */
 
-  /*  useEffect(() => {
-    dispatch(getCoursesScore(5));
-  }, [dispatch]); */
+  const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(0.5),
+  }));
+
+  const coursesTop = useSelector((state) => state.getCourses.setAllCourses);
 
   useEffect(() => {
     dispatch(getCoursesScore(5));
-    setTimeout(nextSlide, 3000); // eslint-disable-next-line
-  }, [slideIndex, dispatch]);
+  }, [dispatch]);
 
   return (
     <div className="container-slider">
-      {images.map((obj, index) => {
-        return (
-          <div
-            key={index}
-            className={slideIndex === index + 1 ? "slide active-anim" : "slide"}
-          >
-            <Paper
-              sx={{
-                p: 2,
-                margin: "auto",
-                maxWidth: 450,
-                elevation: 24,
-                flexGrow: 50,
-                bottom: 0,
-              }}
-            >
-              <Grid container spacing={1}>
-                <Grid>
-                  <ButtonBase>
-                    <Img
-                      alt="complex"
-                      src={obj.img}
-                      width="450px"
-                      height="250px"
-                    />
-                  </ButtonBase>
-                </Grid>
-                <Grid item xs={12} sm container>
-                  <Grid item xs container direction="column" spacing={2}>
-                    <Grid item xs>
-                      <Rating name="read-only" readOnly value={obj.score} />
-                      <Typography gutterBottom variant="h4" component="div">
-                        {obj.title}
-                      </Typography>
-                      <Typography variant="h6" gutterBottom>
-                        {obj.description}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        align="left"
-                        color="text.secondary"
-                      >
-                        Duration:{obj.duration}min
-                      </Typography>
-                      <br />
-                      <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        align="left"
-                      >
-                        <Link className={styles.links} to="/courses">
-                          {obj.categories[0]}
-                        </Link>
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                  <Grid item>
-                    <Typography variant="h5" component="div">
-                      ${obj.price}
-                    </Typography>
-                  </Grid>
+      <Paper
+        sx={{
+          p: 2,
+          margin: "auto",
+          maxWidth: 1200,
+          elevation: 24,
+          flexGrow: 50,
+          bottom: 0,
+        }}
+      >
+        {coursesTop.length > 0 ? (
+          coursesTop.map((c, index) => (
+            <div key={index}>
+              <Grid container xs={4}>
+                <Grid item sx={{ minWidth: 270 }} xs={2} sm={4} md={4}>
+                  {/* <Item sx={{ minWidth: 270 }}> */}
+                  <Card
+                    id={c._id}
+                    title={c.title}
+                    image={c.img}
+                    description={c.description}
+                    score={c.score}
+                    price={c.price}
+                  />
+                  {/* </Item> */}
                 </Grid>
               </Grid>
-            </Paper>
-          </div>
-        );
-      })}
+            </div>
+          ))
+        ) : (
+          <Typography>Cargando</Typography>
+        )}
+      </Paper>
     </div>
   );
 }
--- client/src/components/Carrusel/CarruselPics.js
+++ client/src/components/Carrusel/CarruselPics.js
@@ -16,6 +16,7 @@ export default function CarruselPics() {
   useEffect(() => {
     setTimeout(nextSlide, 3000); // eslint-disable-next-line
   }, [slideIndex]);
+  console.log("ROMAN");
 
   return (
     <div className="container-slider">
