c310793a5899454433403c9d8425052077c94edd
CeciliaWoi <cecilia@woitasen.com.ar>
2021-11-06 23:33:32 -0300

    getReviews TESTEAR

--- api/src/routes/reviewsRoutes.js
+++ api/src/routes/reviewsRoutes.js
@@ -1,15 +1,17 @@
 const { Router } = require("express");
 const express = require("express");
 
+const getAllReviews = require('../controllers/review/getAllReviews')
+
 const createReview = require('../controllers/review/createReview')
-
-
 
 const router = Router();
 router.use(express.json());
 
+// ----> GET <----
+router.get('/', getAllReviews)
+
+// ----> POST <----
 router.post('/createReview', createReview);
-
-
 
 module.exports = router
