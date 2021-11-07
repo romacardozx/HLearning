7c44b90be3186b25315c3ad2879ab1adcbad9f59
gonzalohlh <gonzalo.galdame@gmail.com>
2021-11-08 03:22:44 +0400

    coursesTop corregido

--- client/src/components/Home/CoursesTop.jsx
+++ client/src/components/Home/CoursesTop.jsx
@@ -6,35 +6,34 @@ import Grid from "@mui/material/Grid";
 // import Typography from "@mui/material/Typography";
 import { experimentalStyled as styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
-import Loading from '../Loading/Loading'
-import {getAllCourses, setAllCourses} from '../../redux/actions/getAllCourses'
+import Loading from "../Loading/Loading";
+import { getAllCourses } from "../../redux/actions/getAllCourses";
 
 export default function CoursesTop() {
   const dispatch = useDispatch();
 
-
   const coursesTop = useSelector((state) => state.getCourses.setAllCourses);
-  // const fourCourses = coursesTop.slice(1, 5); 
+  const fourCourses = coursesTop.slice(1, 5);
 
   const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(0.5),
   }));
 
-   useEffect(() => {
+  useEffect(() => {
     dispatch(getCoursesScore(5));
-    dispatch(getAllCourses())
-  }, [dispatch]); 
+    dispatch(getAllCourses());
+  }, [dispatch]);
 
   return (
     <div className="container-slider">
       <Grid container align="center">
-        {coursesTop.length >= 0 ? (
+        {fourCourses.length >= 0 ? (
           <>
             {fourCourses?.map((c, i) => (
               <div key={i}>
                 <Grid item xs={2} sm={4} md={4}>
-                  <Item sx={{ minWidth: 270 }} align='center'>
+                  <Item sx={{ minWidth: 270 }} align="center">
                     <CardTops
                       id={c._id}
                       title={c.title}
@@ -48,7 +47,9 @@ export default function CoursesTop() {
               </div>
             ))}
           </>
-        ) : <Loading />}
+        ) : (
+          <Loading />
+        )}
       </Grid>
       <br />
     </div>
