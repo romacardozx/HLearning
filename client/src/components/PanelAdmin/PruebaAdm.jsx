b666ad455603ab0688637bb650d0a80665b98f3d
Micaela <80907969+MFOSS13@users.noreply.github.com>
2021-11-08 11:33:35 -0300

    pruebas

--- client/src/components/PanelAdmin/PruebaAdm.jsx
+++ client/src/components/PanelAdmin/PruebaAdm.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Navbar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
 import CreateCategory from './CreateCategory';
+import PruebaCat from './PruebaCat';
 
 export default function PruebaAdm(){
     return (
         <div>
-        <Navbar/>
-        <CreateCategory/>
+        <Navbar/><br/><br/><br/><br/>
+        <div>
+        <PruebaCat/>
+        </div><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
         <Footer/>
         </div>
     )
