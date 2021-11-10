60a8e8f2865aa920f938dcc254f435f9fae06cc7
Bellantra <yamilapaez2@hotmail.com>
2021-11-10 14:43:59 +0100

    get all courses sin romper

--- api/src/controllers/courses/getAllCourses.js
+++ api/src/controllers/courses/getAllCourses.js
@@ -17,16 +17,6 @@ const calculeScore = (arrayScores) => {
   return totalScore;
 };
 
-const calculeScore = (arrayScores) => {
-  let sumScore = 0;
-  arrayScores.map((r) => {
-    sumScore = sumScore + parseInt(r.score);
-  });
-
-  const totalScore = sumScore / arrayScores.length;
-
-  return totalScore;
-};
 
 module.exports = async (req, res, next) => {
   try {
