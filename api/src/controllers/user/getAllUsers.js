b296487a2de037e47bc00d2aa999ec611bc6dac3
Bellantra <yamilapaez2@hotmail.com>
2021-11-18 00:18:44 +0100

    populadas rutas de user completas, arreglados mockups email user

--- api/src/controllers/user/getAllUsers.js
+++ api/src/controllers/user/getAllUsers.js
@@ -3,20 +3,20 @@ const Course = require("../../models/Course");
 const Review = require("../../models/Review");
 const Category = require("../../models/Category");
 
+//Todo populado al maximo!
 module.exports = async (_req, res, next) => {
-    try {
-        let users = await User.find({status: "Confirmed"});
-        if(users) {
-            users = await Course.populate(users, {path: "courses"});
-            users = await Category.populate(users, {path: "courses.categories"});
-            users = await Review.populate(users, {path: "reviews"});
-            res.json (users);
-        } else {
-            res.json({msg: "There're any user registred"});
-        }
-    } catch(err) {
-        console.log(err);
-        next(err);
+  try {
+    let users = await User.find({ status: "Confirmed" })
+      .populate("courses")
+      .populate("reviews")
+      .populate("cart");
+    if (users) {
+      res.json(users);
+    } else {
+      res.json({ msg: "There're any user registred" });
     }
-}
-
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
--- api/src/controllers/user/updateUser.js
+++ api/src/controllers/user/updateUser.js
@@ -1,20 +1,22 @@
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 module.exports = async (req, res, next) => {
-    const { id } = req.params;
-    const newDataUser = req.body;
-    try {
-        const user = await User.findOneAndUpdate({_id:id},
-            newDataUser, {
-                new:true
-            });
-        if(user) {
-            res.json(user);
-        } else {
-            res.json({msg: "The user that you're trying to edit doesn't exist"})
-        }
-    } catch(err) {
-        console.log(err);
-        next(err);
+  const { id } = req.params;
+  const newDataUser = req.body;
+  try {
+    const user = await User.findOneAndUpdate({ _id: id }, newDataUser, {
+      new: true,
+    })
+      .populate("courses")
+      .populate("reviews")
+      .populate("cart");
+    if (user) {
+      res.json(user);
+    } else {
+      res.json({ msg: "The user that you're trying to edit doesn't exist" });
     }
-}
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
