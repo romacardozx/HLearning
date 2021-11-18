d89ad741ff6ed005e6a4f51e4e2fe2285ee2b2db
Bellantra <yamilapaez2@hotmail.com>
2021-11-18 17:05:58 +0100

    andando ordenamiento por estrellas en courses creados

--- client/src/utils/calculeScore.js
+++ client/src/utils/calculeScore.js
@@ -1,5 +1,8 @@
 const calculeScore = (arrayScores) => {
   let sumScore = 0;
+
+  if(arrayScores.length === 0) return 0
+  
   arrayScores.map((r) => {
     sumScore = sumScore + parseInt(r.score);
   });
