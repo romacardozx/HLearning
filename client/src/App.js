ed35c475a6f74bfd450a9d28a45c98224c6bf92b
joacocoba <jcaubarrere1@gmail.com>
2021-11-18 01:07:18 -0300

    agrego cambios que faltaban

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import Cart from "./components/Cart/Cart";
 import EditProfile from "./components/UserProfile/EditProfile"
 import PrivateRoute from "./routes/PrivateRoute";
 import AdminRoute from "./routes/AdminRoute"
+import CourseDetail from "./components/PanelAdmin/CourseDetail";
 
 
 function App() {
@@ -42,6 +43,7 @@ function App() {
       <Route exact path="/createcategory" component={CreateCategory} />
       <Route exact path="/cart" component={Cart} />
       <Route exact path="/editprofile" component={EditProfile} />
+      <Route exact path="/courseDetail/:id" component={CourseDetail}/>
     </div>
   );
 }
